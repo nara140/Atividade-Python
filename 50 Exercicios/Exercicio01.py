1a3d015fa7c12607c746182b5e3a477126057d26
nara140 <147212078+nara140@users.noreply.github.com>
2023-10-20 22:28:55 -0300

    Delete 50 Exercicios directory

--- 50 Exercicios/Exercicio01.py
+++
@@ -1,6 +0,0 @@
-#faça um programa que peça uma nota,entre zero e dez.Mostre uma mensagem caso o valor seja inválido e continue pedindo ate que o
-nota=float(input("digite sua nota:"))
-while 0 > nota or 10 < nota:
-    nota=float(input())
-
-
